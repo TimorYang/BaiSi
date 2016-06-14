57bffadf37ecf059a10e13fee14fc4402c40af6b
TimorYang <timoryang@timoryangdemacbook-pro.local>
2016-06-14 18:54:34 +0800

    初始化项目

---
+++ baisiAPP/baisiAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baisiAPP
+//
+//  Created by TimorYang on 16/6/14.
+//  Copyright © 2016年 TimorYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baisiAPP/baisiAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baisiAPP
+//
+//  Created by TimorYang on 16/6/14.
+//  Copyright © 2016年 TimorYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
