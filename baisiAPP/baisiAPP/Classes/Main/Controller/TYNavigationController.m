ba5d6c76bc8ba3675217d3af45df6278569a64fe
TimorYang <timoryang@timoryangdemacbook-pro.local>
2016-06-16 00:24:44 +0800

    1.在NavigationController中统一设置返回按钮,TabBarController的子控制器的根控制器不需要返回按钮,需要做一个判断除了根控制器其他的需要加一个返回按钮

--- baisiAPP/baisiAPP/Classes/Main/Controller/TYNavigationController.m
+++ baisiAPP/baisiAPP/Classes/Main/Controller/TYNavigationController.m
@@ -10,6 +10,7 @@
 
 @implementation TYNavigationController
 
+#pragma mark - 设置当前类的导航条的字体
 //类一加载就设置好他的导航栏的字体
 //谁的事情谁管理
 + (void)load
@@ -24,6 +25,49 @@
     
     //设置背景图片
     [navgationBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
+}
+
+#pragma mark - viewDidLoad
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - pushViewController
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    //非根控制器才需要返回按钮
+    if(self.childViewControllers.count > 0)
+    {
+        //设置导航栏左边的按钮
+        UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        //设置正常状态的图片
+        [backButton setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];
+        //设置高亮状态下的图片
+        [backButton setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
+        //设置文字
+        [backButton setTitle:@"返回" forState:UIControlStateNormal];
+        [backButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        //设置按钮高亮时文字的颜色
+        [backButton setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+        //监听backButton
+        [backButton addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+        //设置按钮尺寸
+        [backButton sizeToFit];
+        //设置按钮的内边距
+        backButton.contentEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 0);
+        //
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:backButton];
+    }
+    //真正的进行跳转
+    [super pushViewController:viewController animated:animated];
+}
+
+#pragma mark - backButton点击事件
+- (void)back
+{
+    //返回上一级
+    [self popViewControllerAnimated:YES];
 }
 
 @end
--- baisiAPP/baisiAPP/Classes/Setting(设置)/Controller/TYSettingController.m
+++ baisiAPP/baisiAPP/Classes/Setting(设置)/Controller/TYSettingController.m
@@ -6,6 +6,15 @@
 //  Copyright © 2016年 TimorYang. All rights reserved.
 //
 
+/*
+ 1.自定义返回按钮替换系统自带的
+    > 自定义按钮,没有尺寸,文字颜色默认是白色
+    > 设置按钮的内边距实现按钮位置的偏移
+    **替换系统的返回按钮后,侧滑返回功能不能使用**
+ 2.统一设置返回按钮
+    > 在NavigationController中设置
+ */
+
 #import "TYSettingController.h"
 
 @implementation TYSettingController
@@ -21,31 +30,8 @@
 #pragma mark - 设置导航栏返回按钮
 - (void)setupBackButton
 {
-    //设置导航栏左边的按钮
-    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    //设置正常状态的图片
-    [backButton setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];
-    //设置高亮状态下的图片
-    [backButton setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
-    //设置文字
-    [backButton setTitle:@"返回" forState:UIControlStateNormal];
-    [backButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    //设置按钮高亮时文字的颜色
-    [backButton setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
-    //监听backButton
-    [backButton addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
-    //设置按钮尺寸
-    [backButton sizeToFit];
-    //设置按钮的内边距
-    backButton.contentEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 0);
-    //
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:backButton];
+    
 }
 
-#pragma mark - backButton点击事件
-- (void)back
-{
-    //返回上一级
-    [self.navigationController popViewControllerAnimated:YES];
-}
+
 @end
